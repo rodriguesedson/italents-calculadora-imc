f8241743666191019fbd1b8f062193fa8f0198f8
Edson Rodrigues <edson091990@gmail.com>
2024-07-11 16:33:04 -0300

    Adiciona comentários no script.js

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
+//Pegar campos peso e altura
+const inputPeso = document.getElementById('peso');
+const inputAltura = document.getElementById('altura');
+
+//Atalhos do teclado (campos da tela de cálculo)
 window.addEventListener('keydown', event => {
   const inputPeso = document.getElementById('peso');
-  
+  //Focar campo "peso"
   if(event.ctrlKey && event.key == 'i') {
     inputPeso.focus();
   }
-
+  //Ativar botão de cálculo
   if(event.key == 'Enter') {
     exibirCalculo();
   }
-
+  //Fechar tela de resultado
   if(event.key == 'Escape') {
     fechar();
   }
 })
 
-const inputPeso = document.getElementById('peso');
-const inputAltura = document.getElementById('altura');
-
+//Cálculo do IMC
 function calcularIMC(peso, altura) {
   if(peso && altura) {
     return (peso / (altura ** 2)).toFixed(1);
@@ -25,6 +28,7 @@ function calcularIMC(peso, altura) {
   }
 }
 
+//Classificação do IMC
 function classificarIMC(valorIMC) {
   if(valorIMC == 0) {
     return "Sem dados";
@@ -39,30 +43,45 @@ function classificarIMC(valorIMC) {
   }
 }
 
+//Exibição do cálculo na tela de resultado
 function exibirCalculo() {
+  //Pegar telas de cálculo, resultado e tabela
   let telaCalculo = document.getElementById('card-calculo');
   let telaResultado = document.getElementById('telaResultado');
   let corpoTabela = document.getElementById('corpo-tabela');
+  //Criar nova linha na tabela com imc e classificação
   let novaLinha = document.createElement('tr');
   let resultadoIMC = document.createElement('td');
   let resultadoClassificacao = document.createElement('td');
+  //Pegar resultado IMC
   let valorIMC = calcularIMC(inputPeso.value, inputAltura.value).toFixed(1);
+  //Pegar classificação IMC
   let classificacaoIMC = classificarIMC(valorIMC);
 
+  //Limpar tabela imc (caso tenha cálculo anterior)
   corpoTabela.innerHTML = '';
+  //Insere valores de cálculo e classificação nos elementos (cálculo/classificação)
   resultadoIMC.textContent = valorIMC;
   resultadoClassificacao.textContent = classificacaoIMC;
+  //Insere cálculo e classificação na linha
   novaLinha.appendChild(resultadoIMC);
   novaLinha.appendChild(resultadoClassificacao);
+  //Insere linha no corpo da tabela
   corpoTabela.appendChild(novaLinha);
+  //Exibe tela de resultado
   telaResultado.style.display = 'block';
+  //Diminui opacidade da tela de cálculo
   telaCalculo.style.opacity = '0.5';
 }
 
+//Fechar tela de resultado
 function fechar() {
+  //oculta tela de resultado
   document.getElementById('telaResultado').style.display = 'none';
+  //Remove valores dos campos peso/altura
   document.getElementById('peso').value = '';
   document.getElementById('altura').value = '';
+  //Restaura nitidez da tela de cálculo
   document.getElementById('card-calculo').style.opacity = '1';
 }
 
@@ -72,7 +91,11 @@ function calcularExemplo(peso, altura) {
   const classificacaoIMC = classificarIMC(valorIMC);
   console.log(`IMC: ${valorIMC} -- Classificação: ${classificacaoIMC}`);
 }
+//Abaixo do peso
 calcularExemplo(55, 1.8);
+//Peso normal
 calcularExemplo(60, 1.8);
+//Sobrepeso
 calcularExemplo(90, 1.8);
+//Obesidade
 calcularExemplo(100, 1.8);
